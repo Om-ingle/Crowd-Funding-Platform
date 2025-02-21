07112681bdb46739c2797bf07e47b48deae20fb4
Om <149283140+Om-ingle@users.noreply.github.com>
2025-02-21 23:14:42 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDdMNls05ihCJRx25lZydUKstS8ZdgBJI0",
-  authDomain: "crowd-funding-platform-18f0b.firebaseapp.com",
-  projectId: "crowd-funding-platform-18f0b",
-  storageBucket: "crowd-funding-platform-18f0b.firebasestorage.app",
-  messagingSenderId: "717609051501",
-  appId: "1:717609051501:web:ea75978cd196f84e346664",
-  measurementId: "G-CCNLTH2ME9"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
