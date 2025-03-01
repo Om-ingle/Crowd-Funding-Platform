56eb3a023f3980b8551ec31df99d283283f0a5ab
Om-ingle <omingle36@gmail.com>
2025-03-01 20:31:13 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+// ...existing code...
+
+// Serve frontend in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../dist')));
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../dist', 'index.html'));
+  });
+}
